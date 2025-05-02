0f6e2654bc1d192124279dc6cd4bbed43087dbef
san1rope <valetinles@gmail.com>
2025-05-02 19:11:53 +0300

    upd

--- tg_bot/misc/startup.py
+++ tg_bot/misc/startup.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.base import StorageKey
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -7,9 +9,13 @@ from tg_bot.db_models.db_gino import connect_to_db
 from tg_bot.db_models.quick_commands import DbTempOrder, TempOrder
 from tg_bot.misc.states import CreateOrder
 
+logger = logging.getLogger(__name__)
+
 
 async def check_user_states():
     await connect_to_db(remove_data=False)
+    logging.basicConfig(level=logging.INFO,
+                        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s')
 
     temp_orders = await DbTempOrder().select()
     for t_order in temp_orders:
@@ -19,4 +25,6 @@ async def check_user_states():
         new_state = FSMContext(storage=MemoryStorage(), key=new_key)
 
         state_to_set = getattr(CreateOrder, t_order.current_state)
+        print(f"state_to_set = {state_to_set}")
         await new_state.set_state(state_to_set)
+        logging.info(f"Установил состояние для {t_order.tg_user_id}")
